0b40c47c4418d057eb994df93672af3d6a04d14b
DavideTonin99 <davide9935@gmail.com>
2017-11-29 20:25:01 +0100

    urls fix

--- djfantamarconi/urls.py
+++ djfantamarconi/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.shortcuts import HttpResponseRedirect, reverse
 
 urlpatterns = [
-    url(r'^$', lambda request: HttpResponseRedirect(reverse('fantamarconi:home'))),
+    url(r'^$', lambda request: HttpResponseRedirect(reverse('home'))),
 	url(r'^', include('fantamarconi.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^fantamarconi/', include('fantamarconi.urls'))
