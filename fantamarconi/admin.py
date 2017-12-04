fe1bbeb35023138d311dab4e59fd257e72ceeb0f
DavideTonin99 <davide9935@gmail.com>
2017-12-04 09:45:29 +0100

    Added Timeline model

--- fantamarconi/admin.py
+++ fantamarconi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Processes
+from .models import Processes, Timeline
 
 # Register your models here.
 admin.site.register(Processes)
+admin.site.register(Timeline)
--- fantamarconi/models.py
+++ fantamarconi/models.py
@@ -11,3 +11,21 @@ class Processes(models.Model):
     start_date = models.DateField(auto_now=False, null=True, blank=True)
     end_date = models.DateField(auto_now=False, null=True, blank=True)
     referent = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Timeline(models.Model):
+
+    class Meta:
+        unique_together = (('process', 'referent', 'start_date'),)
+
+    process = models.ForeignKey('Processes')
+    referent = models.ForeignKey(settings.AUTH_USER_MODEL)
+    start_date = models.DateField(auto_now=False, null=True, blank=True)
+    end_date = models.DateField(auto_now=False, null=True, blank=True)
+    job = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return ", ".join([self.process.name, self.referent.username, self.job])
